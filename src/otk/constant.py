34d9472be3aee810784bd81aee8e71133ba9f90f
Simon de Vlieger <cmdr@supakeen.com>
2024-05-14 11:15:47 +0200

    directive: allow tags
    This changes (some) directives to allow trailing `.tag`'s to work around
    the issue of duplicate keys not being available.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- src/otk/constant.py
+++ src/otk/constant.py
@@ -4,6 +4,7 @@ PREFIX_TARGET = f"{PREFIX}target."
 PREFIX_OP = f"{PREFIX}op."
 PREFIX_CUSTOMIZATION = f"{PREFIX}customization."
 
+PREFIX_INCLUDE = f"{PREFIX}include"
+PREFIX_DEFINE = f"{PREFIX}define"
+
 NAME_VERSION = f"{PREFIX}version"
-NAME_INCLUDE = f"{PREFIX}include"
-NAME_DEFINE = f"{PREFIX}define"
