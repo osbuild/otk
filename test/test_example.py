57d5d4ff21a3a8ca7baf76275cff1f09df69af07
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-16 20:07:49 +0200

    test: error check to test_example
    Add a test that produces an error with an expected message.  For now it
    only tests that a file not found error includes the name of the source
    file in the message.

--- test/test_example.py
+++ test/test_example.py
@@ -19,3 +19,14 @@ def test_command_compile_on_base_examples(tmp_path, src_yaml):
     expected = json.load(src_yaml.with_suffix(".json").open())
     actual = json.load(dst.open())
     assert expected == actual
+
+
+@pytest.mark.parametrize("src_yaml",
+                         [str(path) for path in (pathlib.Path(__file__).parent / "data/error").glob("*.yaml")])
+def test_errors(src_yaml):
+    src_yaml = pathlib.Path(src_yaml)
+    expected = src_yaml.with_suffix(".err").read_text().strip()
+    ns = argparse.Namespace(input=src_yaml, output="/dev/null", target="osbuild")
+    with pytest.raises(Exception) as exception:
+        command.compile(ns)
+    assert expected in str(exception.value)
