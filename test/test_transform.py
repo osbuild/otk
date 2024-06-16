6276058ff592d3514568bd6d0516ef06b572520a
Michael Vogt <michael.vogt@gmail.com>
2024-06-16 09:09:05 +0200

    transform: make recursion more explicit
    This commit makes the recursive `resolve()`function more explicit,
    i.e. by having an explicit `if/elif/else` it is more visible what
    parts of the recursion will go deeper and what parts will exit.

--- test/test_transform.py
+++ test/test_transform.py
@@ -2,14 +2,6 @@ from otk import transform
 from otk.context import CommonContext
 
 
-def test_dont_resolve():
-    assert transform.dont_resolve(CommonContext(), 1) == 1
-    assert transform.dont_resolve(CommonContext(), 1.0) == 1.0
-    assert transform.dont_resolve(CommonContext(), "foo") == "foo"
-    assert transform.dont_resolve(CommonContext(), True)
-    assert transform.dont_resolve(CommonContext(), None) is None
-
-
 def test_resolve_list():
     assert transform.resolve_list(
         CommonContext(),
