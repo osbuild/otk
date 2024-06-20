fafd6c32daeeb7a77f5d2ed6dadfe89b62282ac8
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-20 16:55:55 +0200

    otk: add defines and copy() method to traversal.State
    The defines property of the State will be used to keep track of
    subblocks of variable definitions when processing otk.define directives.
    
    The copy() method creates a new State with replaced or copied
    properties.
    
    This will enable us to create State objects with references to
    sub-objects of the global defines variables.  While processing defines
    sub-objects, the State's defines can be written to to define values
    nested under the key being processed, while using the global Context
    object to resolve variable names inside defines.

--- src/otk/traversal.py
+++ src/otk/traversal.py
@@ -1,3 +1,20 @@
 class State:
-    def __init__(self, path):
+    def __init__(self, path, defines):
         self.path = path
+        self.defines = defines
+
+    def copy(self, *, path=None, defines=None) -> "State":
+        """
+        Return a new State, optionally redefining the path and defines
+        properties. Properties not defined in the args are (shallow) copied
+        from the existing instance.
+        A shallow copy of the 'defines' allows us to keep modifying the global
+        context variables through the reference on this State object, which can
+        refer to a sub-object of the global context when necessary.
+        """
+        if path is None:
+            path = self.path
+        if defines is None:
+            defines = self.defines
+
+        return State(path, defines)
