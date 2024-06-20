c6be957126f17fe5900f793e95cca0cddff81a07
Achilleas Koutsou <achilleas@koutsou.net>
2024-06-20 12:39:46 +0200

    test: adjust substitute_vars() tests
    Rename the tests and update the expected errors to match the new error
    message.
    A new test case is added for a dictionary replacement error in a
    substring as well.

--- test/test_substitute_vars.py
+++ test/test_substitute_vars.py
@@ -1,35 +1,38 @@
+import re
+
 import pytest
-
 from otk.context import CommonContext
-from otk.error import TransformDirectiveTypeError
 from otk.directive import substitute_vars
+from otk.error import TransformDirectiveTypeError
 
 
-def test_simple_sugar():
+def test_simple_sub_var():
     context = CommonContext()
     context.define("my_var", "foo")
 
     assert substitute_vars(context, "${my_var}") == "foo"
 
 
-def test_simple_sugar_nested():
+def test_simple_sub_var_nested():
     context = CommonContext()
     context.define("my_var", [1, 2])
 
     assert substitute_vars(context, "${my_var}") == [1, 2]
 
 
-def test_simple_sugar_nested_fail():
+def test_simple_sub_var_nested_fail():
     context = CommonContext()
     context.define("my_var", [1, 2])
+    data = "a${my_var}"
 
-    expected_error = "string sugar resolves to an incorrect type, expected int, float, or str but got %r"
+    expected_error = re.escape(f"string '{data}' resolves to an incorrect type, "
+                               "expected int, float, or str but got list")
 
     with pytest.raises(TransformDirectiveTypeError, match=expected_error):
-        substitute_vars(context, "a${my_var}")
+        substitute_vars(context, data)
 
 
-def test_sugar_multiple():
+def test_sub_var_multiple():
     context = CommonContext()
     context.define("a", "foo")
     context.define("b", "bar")
@@ -37,13 +40,27 @@ def test_sugar_multiple():
     assert substitute_vars(context, "${a}-${b}") == "foo-bar"
 
 
-def test_sugar_multiple_fail():
+def test_sub_var_multiple_fail():
     context = CommonContext()
     context.define("a", "foo")
     context.define("b", [1, 2])
+    context.define("c", {"one": 1})
+    data = "${a}-${b}"
 
-    expected_error = "string sugar resolves to an incorrect type, expected int, float, or str but got %r"
+    # the a will be replaced but the b will cause an error
+    expected_error = re.escape(r"string 'foo-${b}' resolves to an incorrect type, "
+                               "expected int, float, or str but got list")
 
     # Fails due to non-str type
     with pytest.raises(TransformDirectiveTypeError, match=expected_error):
-        substitute_vars(context, "${a}-${b}")
+        substitute_vars(context, data)
+
+    data = "${a}-${c}"
+
+    # the a will be replaced but the c will cause an error
+    expected_error = re.escape(r"string 'foo-${c}' resolves to an incorrect type, "
+                               "expected int, float, or str but got dict")
+
+    # Fails due to non-str type
+    with pytest.raises(TransformDirectiveTypeError, match=expected_error):
+        substitute_vars(context, data)
