731df4e2097310f4e449ecb276400bc9176c2301
Michael Vogt <michael.vogt@gmail.com>
2024-06-24 16:16:08 +0200

    otk: simplify Omnifest and use `process_include` in entrypoint
    With the recent change to require a fully processed tree before
    creating the omnifest [0] it seems we can simplify the Omnifest
    a bit more now that it essentially is just a container for an
    externally resolved tree.
    
    This commit does that by removing some of the entrypoints
    like `from_yaml_{file,data}` as those are not that useful
    without resolving the tree first.
    
    One nice effect of this change is that the circular import
    detection now includes the top-level entrypoint in the error
    and it is also more symetrical (a single way to open/read/parse
    an otk file).
    
    The handling of data from <stdin> is not super nice right now
    and it actually makes me wonder if we should maybe stop supporting
    omnifests from stdin. It seems that most omnifests will contain
    includes so the notion of creating an omnifest from <stdin> is
    a bit fuzzy.
    
    [0] https://github.com/osbuild/otk/pull/128

--- test/test_document.py
+++ test/test_document.py
@@ -1,23 +1,7 @@
-import textwrap
-
 import pytest
 
 from otk.document import Omnifest
-from otk.error import ParseTypeError, ParseVersionError
-
-
-def test_omnifest_read_dict():
-    assert Omnifest.read({}) == {}
-
-
-def test_omnifest_read_list():
-    with pytest.raises(ParseTypeError):
-        Omnifest.read([])
-
-
-def test_omnifest_read_string():
-    with pytest.raises(ParseTypeError):
-        Omnifest.read("")
+from otk.error import ParseVersionError
 
 
 def test_omnifest_ensure():
@@ -27,70 +11,3 @@ def test_omnifest_ensure():
 def test_omnifest_ensure_no_keys():
     with pytest.raises(ParseVersionError):
         assert Omnifest.ensure({})
-
-
-def test_omnifest_from_yaml_bytes_sad():
-    # A bunch of YAMLs that don't contain a top level map
-    with pytest.raises(ParseTypeError):
-        Omnifest.from_yaml_bytes(
-            """
-"""
-        )
-
-    with pytest.raises(ParseTypeError):
-        Omnifest.from_yaml_bytes(
-            """
-1
-"""
-        )
-
-    with pytest.raises(ParseTypeError):
-        Omnifest.from_yaml_bytes(
-            """
-- 1
-- 2
-"""
-        )
-
-    with pytest.raises(ParseTypeError):
-        Omnifest.from_yaml_bytes(
-            """
-"1"
-"""
-        )
-
-    # And a YAML that does have a top level map, but no `otk.version` key
-    with pytest.raises(ParseVersionError):
-        Omnifest.from_yaml_bytes(
-            """
-otk.not-version: 1
-"""
-        )
-
-
-TEST_CASES = [
-    (
-        textwrap.dedent("""
-        otk.version: 1
-        otk.target.osbuild:
-          some: key
-        """),
-        {"otk.target.osbuild": {"some": "key"}, "otk.version": 1},
-    )
-]
-
-
-@pytest.mark.parametrize("fake_input,expected_tree", TEST_CASES)
-def test_omnifest_from_yaml_bytes_happy(fake_input, expected_tree):
-    omi = Omnifest.from_yaml_bytes(fake_input)
-    assert omi.tree == expected_tree
-
-
-@pytest.mark.parametrize("fake_input,expected_tree", TEST_CASES)
-def test_omnifest_from_yaml_file_happy(tmp_path, fake_input, expected_tree):
-    fake_yaml_path = tmp_path / "test.otk"
-    fake_yaml_path.write_text(fake_input)
-
-    with fake_yaml_path.open() as fp:
-        omi = Omnifest.from_yaml_file(fp)
-    assert omi.tree == expected_tree
