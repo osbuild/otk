a69bb1eb377da2fcc9d4aae63d903f1426a4b12e
Michael Vogt <michael.vogt@gmail.com>
2024-06-26 09:57:10 +0200

    context: bring back override warnings
    This commit brings back the var override warnings, they will probably
    be useful for debugging. The format is little funky when nested
    vars get replaced that used to be values and need to become dicts.
    E.g.
    ```
    key:
     sub: subval
    ```
    and with something like:
    ```
    ctx.define("key.sub.subsub.subsubsub", {"subsubsub": "val2"})
    ```
    the generated warning will generate a doted notation in the
    dict which is a bit strange but probably good enough:
    ```
    redefinition of 'key.sub', previous values was 'subval' and new value is {'subsub.subsubsub': {'subsubsub': 'val2'}}
    ```
    The alternative would be to construct the full dict which is not
    hard but probably also does not add too much extra clarity(?).
    
    Co-authored-by: Simon de Vlieger <supakeen@redhat.com>

--- test/test_context.py
+++ test/test_context.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 from otk.context import CommonContext
@@ -34,13 +36,15 @@ def test_context():
     assert ctx.variable("boo.1") == 2
 
 
-def test_context_define_subkey():
+def test_context_define_subkey(caplog):
+    caplog.set_level(logging.WARNING)
     ctx = CommonContext()
     ctx.define("key", "val")
     assert ctx.variable("key") == "val"
 
     ctx.define("key.subkey", "subval")
     assert ctx.variable("key") == {"subkey": "subval"}
+    assert len(caplog.records) == 0
     ctx.define("key.subkey2", "subval2")
     assert ctx.variable("key") == {"subkey": "subval", "subkey2": "subval2"}
     ctx.define("key", "other-val")
@@ -48,6 +52,42 @@ def test_context_define_subkey():
 
     ctx.define("other.key.with.subkey", "subval")
     assert ctx.variable("other") == {"key": {"with": {"subkey": "subval"}}}
+
+
+def test_context_warn_on_override_simple(caplog):
+    caplog.set_level(logging.WARNING)
+    ctx = CommonContext(duplicate_definitions_warning=True)
+    ctx.define("key", "val")
+    assert len(caplog.records) == 0
+    ctx.define("key", "new-val")
+    expected_msg = "redefinition of 'key', previous values was 'val' and new value is 'new-val'"
+    assert [expected_msg] == [r.message for r in caplog.records]
+
+
+def test_context_warn_on_override_nested(caplog):
+    caplog.set_level(logging.WARNING)
+    ctx = CommonContext(duplicate_definitions_warning=True)
+    ctx.define("key.subkey.subsubkey", "subsubval")
+    assert len(caplog.records) == 0
+    ctx.define("key.subkey", "newsubval")
+    # from dict -> str
+    expected_msg1 = "redefinition of 'key.subkey', previous values was {'subsubkey': 'subsubval'} and new value is 'newsubval'"
+    assert [expected_msg1] == [r.message for r in caplog.records]
+    ctx.define("key.subkey", {"sub": "dict"})
+    # from str -> dict
+    expected_msg2 = "redefinition of 'key.subkey', previous values was 'newsubval' and new value is {'sub': 'dict'}"
+    assert [expected_msg1, expected_msg2] == [r.message for r in caplog.records]
+
+
+def test_context_warn_on_override_nested_from_val_to_dict(caplog):
+    caplog.set_level(logging.WARNING)
+    ctx = CommonContext(duplicate_definitions_warning=True)
+    ctx.define("key.sub", "subval")
+    assert len(caplog.records) == 0
+    ctx.define("key.sub.subsub.subsubsub", {"subsubsub": "val2"})
+    expected_msg = "redefinition of 'key.sub', previous values was 'subval' and new value is {'subsub.subsubsub': {'subsubsub': 'val2'}}"
+
+    assert [expected_msg] == [r.message for r in caplog.records]
 
 
 def test_context_nonexistent():
