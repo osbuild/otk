39e198da954c1253f5df5f67d3e08c4d2009a82d
Michael Vogt <michael.vogt@gmail.com>
2024-09-02 13:59:59 +0200

    [RFC] test: test against reference images lib generated manifest
    This is an idea how to make development of the otk manifest
    easier to iterate on. The idea is that we filter out the
    sources/rpms (we should consider later to keep the rpm names but
    not the exact versions) and then compare the stages. This way
    we can iterate on the development of the centos-9 example and
    always be confident that the reference is matching.
    
    The reference was generated with the `images` library.

---
+++ test/test_image_ref.py
@@ -0,0 +1,26 @@
+import json
+import os
+import pathlib
+import yaml
+
+import pytest
+
+from otk.command import run
+
+
+def test_images_ref(tmp_path):
+    # XXX: use pytest.parameterize and load all ref images automatically
+    # and compare
+    ref_yaml = pathlib.Path("test/data/images-ref/centos-9-x86_64-qcow2.ref.yaml")
+    with ref_yaml.open() as fp:
+        ref_manifest = yaml.safe_load(fp)
+        
+    manifest_json = tmp_path / "manifest.json"
+    run(["compile", "-o", os.fspath(manifest_json),
+         "example/centos/centos-9-x86_64-qcow2.yaml"])
+    with manifest_json.open() as fp:
+        manifest = json.load(fp)
+    # XXX: filter rpm/curl stage before comparing
+
+    assert manifest == ref_manifest
+
