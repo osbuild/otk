6ad7247adac78bcad01d87ccbb46689dca0bacd9
Michael Vogt <michael.vogt@gmail.com>
2024-09-03 10:47:33 +0200

    example: import centos-9-x86_64-ami

--- test/test_image_ref.py
+++ test/test_image_ref.py
@@ -8,19 +8,20 @@ import pytest
 from otk.command import run
 
 
-def test_images_ref(tmp_path):
+@pytest.mark.parametrize("ref_yaml",
+                         [path for path in (pathlib.Path(__file__).parent / "data/images-ref").glob("*.yaml")])
+def test_images_ref(tmp_path, ref_yaml):
     os.environ["OSBUILD_TESTING_RNG_SEED"] = "0"
 
-    # XXX: use pytest.parameterize and load all ref images automatically
-    # and compare
-    ref_yaml = pathlib.Path("test/data/images-ref/centos-9-x86_64-qcow2.ref.yaml")
     with ref_yaml.open() as fp:
         ref_manifest = yaml.safe_load(fp)
-        
+
+    src_yaml = pathlib.Path("example/centos") / ref_yaml.name
     manifest_json = tmp_path / "manifest.json"
     run(["compile",
          "-o", os.fspath(manifest_json),
-         "example/centos/centos-9-x86_64-qcow2.yaml"])
+         os.fspath(src_yaml),
+         ])
     with manifest_json.open() as fp:
         manifest = json.load(fp)
     # XXX: filter rpm/curl stage before comparing
