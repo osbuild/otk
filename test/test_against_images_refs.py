d5ef4f6c6c5c6fe72a8e01171b44d4315a58356c
Michael Vogt <michael.vogt@gmail.com>
2024-09-23 17:27:18 +0200

    Revert "test: ignore UUIDs"
    This reverts commit b87cba396004fc893c790441d05b1f3f41b351da.

--- test/test_against_images_refs.py
+++ test/test_against_images_refs.py
@@ -1,7 +1,6 @@
 import json
 import os
 import pathlib
-import re
 import yaml
 
 import pytest
@@ -88,10 +87,6 @@ def test_images_ref(tmp_path, monkeypatch, tc):
         ref_manifest = yaml.safe_load(fp)
         normalize_rpm_refs(ref_manifest)
 
-    # hack
-    ref_manifest = json.loads(
-        re.sub(r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", "", json.dumps(ref_manifest)))
-
     otk_json = tmp_path / "manifest-otk.json"
     run(["compile",
          "-o", os.fspath(otk_json),
@@ -100,9 +95,5 @@ def test_images_ref(tmp_path, monkeypatch, tc):
     with otk_json.open() as fp:
         manifest = json.load(fp)
         normalize_rpm_refs(manifest)
-
-    # hack
-    manifest = json.loads(
-        re.sub(r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", "", json.dumps(manifest)))
 
     assert manifest == ref_manifest
