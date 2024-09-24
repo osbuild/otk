46ae7ab727fd70e14f3568df3fe1ff59ee637228
Simon de Vlieger <supakeen@redhat.com>
2024-09-24 10:07:31 +0200

    external/depsolve: allow passing options for rpm
    Allow the passing of options for the RPM stage. Some of our omnifests
    will need to do this (e.g. `edge-commit` turns off `dracut` and some
    other bits and bobs).
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- src/otk_external_osbuild/command/make_depsolve_dnf4_rpm_stage.py
+++ src/otk_external_osbuild/command/make_depsolve_dnf4_rpm_stage.py
@@ -5,7 +5,15 @@ from typing import TextIO
 
 def root(input_stream: TextIO) -> None:
     data = json.load(input_stream)
-    tree = data["tree"]["packageset"]["const"]["internal"]
+    tree = data["tree"]
+    pkgs = tree["packageset"]["const"]["internal"]
+
+    # allow passing on rpm stage options
+    opts = tree.get("options", {}).get("rpm_stage", {})
+
+    # perhaps gpgkeys should move *under* options?
+    # it should: https://github.com/osbuild/otk/issues/218
+    opts["gpgkeys"] = tree["gpgkeys"]
 
     sys.stdout.write(
         json.dumps(
@@ -20,13 +28,11 @@ def root(input_stream: TextIO) -> None:
                                 {
                                     "id": package["checksum"],
                                 }
-                                for package in tree["packages"]
+                                for package in pkgs["packages"]
                             ],
                         },
                     },
-                    "options": {
-                        "gpgkeys": data["tree"]["gpgkeys"],
-                    },
+                    "options": opts,
                 }
             }
         )
