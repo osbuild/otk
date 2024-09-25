0e00f3b7bd9ea66ced3b75c84258226fbc5ed8b3
Michael Vogt <michael.vogt@gmail.com>
2024-09-25 15:51:53 +0200

    external: add new `get-dnf4-package-info` external
    This commit adds a new `get-dnf4-package-info` external that can
    be used to extract details about a dnf4 resolved package.
    
    The main use-case is the kernel package where we need some details
    like the version number to put into the grub stage. With this
    commit we can do something like:
    ```yaml
      kernel:
        package:
          otk.external.osbuild-get-dnf4-package-info:
            packageset: ${packages.os}
            packagename: ${modifications.kernel_package_name}
    ```

---
+++ src/otk_external_osbuild/command/get_dnf4_package_info.py
@@ -0,0 +1,39 @@
+import json
+import sys
+from typing import List, Optional, TextIO
+
+
+def find_pkg_by_name(packages: List[dict], pkg_name: str) -> Optional[dict]:
+    for pkg in packages:
+        if pkg["name"] == pkg_name:
+            return pkg
+    return None
+
+
+def root(input_stream: TextIO) -> None:
+    data = json.load(input_stream)
+    pkg_name = data["tree"]["packagename"]
+    packages = data["tree"]["packageset"]["const"]["internal"]["packages"]
+
+    pkg = find_pkg_by_name(packages, pkg_name)
+    if not pkg:
+        raise KeyError(f"cannot find package {pkg_name}")
+    sys.stdout.write(
+        json.dumps(
+            {
+                "tree": {
+                    "version": pkg["version"],
+                    "release": pkg["release"],
+                    "arch": pkg["arch"],
+                }
+            }
+        )
+    )
+
+
+def main():
+    root(sys.stdin)
+
+
+if __name__ == "__main__":
+    main()
