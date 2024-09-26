cf860e7d7e3af3bf012fd2297f8092095a31b934
Simon de Vlieger <supakeen@redhat.com>
2024-09-26 11:57:27 +0200

    test: depsolve rpm stage with options test
    Test generation of the RPM stage *with* options.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- test/test_make_depsolve_dnf4_rpm_stage.py
+++ test/test_make_depsolve_dnf4_rpm_stage.py
@@ -58,3 +58,65 @@ def test_make_depsolve_dnf4_rpm_stage(capsys):
             }
         }
     }
+
+
+fake_input_with_options = {
+    "tree": {
+        "packageset": {
+            "const": {
+                "internal": {
+                    "packages": [
+                        {
+                            "remote_location": "http://example.com/pkg1",
+                            "checksum": "sha256:1234",
+                        },
+                        {
+                            "remote_location": "http://example.com/pkg2",
+                            "checksum": "sha256:5678",
+                        }
+                    ]
+                }
+            }
+        },
+        "gpgkeys": [
+            "gpg-key1"
+        ],
+        "options": {
+            "rpm_stage": {
+                "dbpath": "/usr/share/rpm",
+                "disable_dracut": True,
+            }
+        }
+    }
+}
+
+
+def test_make_depsolve_dnf4_rpm_stage_with_options(capsys):
+    root(StringIO(json.dumps(fake_input_with_options)))
+    output = json.loads(capsys.readouterr().out)
+    assert output == {
+        "tree": {
+            "type": "org.osbuild.rpm",
+            "inputs": {
+                "packages": {
+                    "type": "org.osbuild.files",
+                    "origin": "org.osbuild.source",
+                    "references": [
+                        {
+                            "id": "sha256:1234",
+                        },
+                        {
+                            "id": "sha256:5678",
+                        }
+                    ]
+                }
+            },
+            "options": {
+                "gpgkeys": [
+                    "gpg-key1",
+                ],
+                "disable_dracut": True,
+                "dbpath": "/usr/share/rpm",
+            }
+        }
+    }
