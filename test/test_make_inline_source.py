e30b076eb3747d285d77ad8cf30d8cea416f7cab
Achilleas Koutsou <achilleas@koutsou.net>
2024-09-27 17:19:31 +0200

    test: add test for `make-inline-source`

---
+++ test/test_make_inline_source.py
@@ -0,0 +1,29 @@
+import json
+from io import StringIO
+from test.test_gen_inline_files import expected_output
+
+from otk_external_osbuild.command.make_inline_source import root
+
+# use the output from test_gen_inline_files
+test_input = expected_output
+
+
+def test_make_inline_source(capsys):
+    root(StringIO(json.dumps(test_input)))
+    output = json.loads(capsys.readouterr().out)
+    assert output == {
+        "tree": {
+            "org.osbuild.inline": {
+                "items": {
+                    "sha256:5e557cb2cceecc1845e8210350c4aa09d2925a3bfbe54c59c10e02e3d03555f6": {
+                        "encoding": "base64",
+                        "data": "SGVsbG8gQm9iCkhvdydzIGl0IGdvaW5nPwo=",
+                    },
+                    "sha256:0f26599ad3142e51c0781c93677eba62b4a28db327bc9eb443aaa764976045de": {
+                        "encoding": "base64",
+                        "data": "TGV0J3MgdGVzdCB0aGUgaW5saW5lIGZpbGUgZ2VuZXJhdG9yLg==",
+                    },
+                }
+            }
+        }
+    }
