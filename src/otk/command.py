db5626a7774d040f8fdf9cee23449f48242f4687
Simon de Vlieger <supakeen@redhat.com>
2024-10-10 09:48:12 +0200

    otk/ui: provide and use a print function
    Only prints output when stderr is a tty.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

--- src/otk/command.py
+++ src/otk/command.py
@@ -5,6 +5,7 @@ import sys
 from typing import List
 
 from .document import Omnifest
+from . import ui
 
 log = logging.getLogger(__name__)
 
@@ -41,6 +42,8 @@ def _process(arguments: argparse.Namespace, dry_run: bool) -> int:
     else:
         path = pathlib.Path(arguments.input)
 
+    ui.print(f"Compiling {path}")
+
     # First pass of resolving the otk file is "shallow", it will not run
     # externals and not resolve anything under otk.target.*
     #
@@ -62,11 +65,15 @@ def _process(arguments: argparse.Namespace, dry_run: bool) -> int:
         log.fatal("requested target %r does not exist in INPUT", target_requested)
         return 1
 
+    ui.print(f"Selected the {target_requested} target")
+
     # Now do the real resolve that takes the target into account. It needs
     # a full run so that resolving includes works correctly.
     warn_duplicated_defs = any(arg in getattr(arguments, "warn", [])
                                for arg in ["duplicate-definition", "all"])
     doc = Omnifest(path, target=target_requested, warn_duplicated_defs=warn_duplicated_defs)
+
+    ui.print("Done")
 
     # and then output by writing to the output
     if not dry_run:
