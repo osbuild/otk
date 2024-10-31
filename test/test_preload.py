3c2044563f0d2aaecd7a3b59999022bd7e3f0c20
Simon de Vlieger <supakeen@redhat.com>
2024-10-31 11:49:22 +0100

    otk: test multiple omnifests (preload)
    Introduce the testcase written by Michael for another PR. This tests
    supplying multiple files and using variables from both.
    
    Co-authored-by: Michael Vogt <michael.vogt@gmail.com>
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ test/test_preload.py
@@ -0,0 +1,38 @@
+import os
+import textwrap
+
+from otk.command import run
+
+
+def test_otk_preload_works(tmp_path):
+    expected_json = textwrap.dedent("""\
+    {
+      "lang": "nl_NL.UTF-8",
+      "version": "2"
+    }""")
+    preload_otk = tmp_path / "preload.yaml"
+    preload_otk.write_text(textwrap.dedent("""
+    otk.define:
+      user:
+        modifications:
+          language: nl_NL.UTF-8
+    """))
+
+    test_otk = tmp_path / "foo.yaml"
+    test_otk.write_text(textwrap.dedent("""
+    otk.version: 1
+    otk.define:
+      default:
+        modifications:
+          language: en_US.UTF-8
+      modifications:
+        otk.op.join:
+          values:
+            - ${default.modifications}
+            - ${user.modifications}
+    otk.target.osbuild:
+      lang: ${modifications.language}
+    """))
+    result_otk = tmp_path / "output.yaml"
+    run(["compile", os.fspath(preload_otk), os.fspath(test_otk), "-o", result_otk.as_posix()])
+    assert expected_json == result_otk.read_text()
